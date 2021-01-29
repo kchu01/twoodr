54590607a8b1d95b1efb5b89dacc2e0ca2c1d317
kchu01 <chukathy01@gmail.com>
2021-01-28 17:05:21 -0800

    Create Twood Update1

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-document.querySelector('#submit-twood').addEventListener('click', () => {
-  const newText = document.querySelector('#twood-input').value
-  // document.querySelector('.twood').innerText = newText
-  document.querySelector('#twood-input').value = ''
 
-  const newTwood = document.createElement('div')
-  document.querySelector('.twoods-container').append(newTwood)
-  // newTwood.classList.add('twood')
-  // newTwood.innerText = newText
+// Work
+const newTwood = document.querySelector('#twood-input').value;
+
+const button = document.querySelector('#submit-twood');
+button.addEventListener('click', function () {
+  console.log('Button clicked!')
+
+const showTwood = document.querySelector('.twood')
+
+showTwood.innerText = newTwood
+} )
+
+var twoodTimer = document.querySelector('.cutoff.hidden')
+
+console.log(twoodTimer)
+
+setTimeout(function(){
+  document.querySelector('.cutoff').classList.remove('hidden');
+}, 200000);
 
 
-})
 
-setTimeout(() => {
-  document.querySelector('.cutoff').classList.remove('hidden')  
-}, 200000)
+// const alertFunc() {
+//   alert("Time is up!");
+// }
+
+
+// setTimeout(function(){
+// document.write("Session Timed Out. Goodbye Twooder!");
+// }, 20000);
+
+
+newTwood.classList.add('twood');
+newTwood.innerText = newText
